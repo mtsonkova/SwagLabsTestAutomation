8497a4abeb804e0d7ccaef81c12e18915903f564
mtsonkova <mtsonkova950@gmail.com>
2023-12-30 22:24:22 +0200

    In progress

---
+++ src/test/java/TestLoginPage.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestLoginPage {
+
+    public WebDriver driver;
+    @BeforeTest(alwaysRun = true)
+    public void setUp() {
+        driver = new ChromeDriver();
+        String baseUrl = "https://www.saucedemo.com/";
+        driver.get(baseUrl);
+    }
+
+    @Test
+    public void LoginWithValidUserNameAndPasswordAndCheckPageTltle() {
+        WebElement usernameField = driver.findElement(By.id("user-name"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement btn_login = driver.findElement(By.id("login-button"));
+
+        usernameField.sendKeys("standard_user");
+        passwordField.sendKeys("secret_sauce");
+        btn_login.click();
+
+        String expectedUrl = "https://www.saucedemo.com/inventory.html";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+    }
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
