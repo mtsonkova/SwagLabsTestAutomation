493d8330b0af9b242a9821721aaf773bb4dcd85e
mtsonkova <mtsonkova950@gmail.com>
2024-02-09 23:39:37 +0200

    In progress

--- src/test/java/E2ETests.java
+++ src/test/java/E2ETests.java
@@ -1,11 +1,18 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.sql.SQLOutput;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
 public class E2ETests extends BaseTest {
 
+    By storeItems = By.className("inventory_item_name");
     @Test
     public void PurchaseAllProductsAndCheckOrder() {
         WebElement usernameField = driver.findElement(By.id("user-name"));
@@ -16,7 +23,19 @@ public class E2ETests extends BaseTest {
         passwordField.sendKeys("secret_sauce");
         btn_login.click();
 
-        WebDriverWait wait = new WebDriverWait(driver, D)
 
-    }
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(storeItems));
+
+        List<WebElement> inventoryItemsLayout = driver.findElements(By.className("inventory_item_description"));
+
+       // Add all elements to cart
+       for(WebElement element : inventoryItemsLayout) {
+
+           WebElement priceBar = element.findElement(By.cssSelector(".pricebar"));
+            WebElement btn_AddToCart = priceBar.findElement(By.linkText("Add to cart"));
+            btn_AddToCart.click();
+        }
+        System.out.println("All items added to the shopping cart");
+        }
 }
