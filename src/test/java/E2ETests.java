d73666f5eaa94d71ca3272c21a719919d5f3bb01
mtsonkova <mtsonkova950@gmail.com>
2024-02-13 21:52:25 +0200

    E2E test case completed

--- src/test/java/E2ETests.java
+++ src/test/java/E2ETests.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.sql.SQLOutput;
@@ -32,10 +33,34 @@ public class E2ETests extends BaseTest {
        // Add all elements to cart
        for(WebElement element : inventoryItemsLayout) {
 
-           WebElement priceBar = element.findElement(By.cssSelector(".pricebar"));
-            WebElement btn_AddToCart = priceBar.findElement(By.linkText("Add to cart"));
+          // WebElement priceBar = element.findElement(By.cssSelector(".pricebar"));
+            WebElement btn_AddToCart = element.findElement(By.tagName("button"));
             btn_AddToCart.click();
         }
-        System.out.println("All items added to the shopping cart");
+        WebElement shopingCartIcon = driver.findElement(By.className("shopping_cart_link"));
+       shopingCartIcon.click();
+
+       WebElement btn_Checkout = driver.findElement(By.id("checkout"));
+       btn_Checkout.click();
+
+       WebElement firstNameInput = driver.findElement(By.id("first-name"));
+       WebElement lastNameInput = driver.findElement(By.id("last-name"));
+       WebElement postalCodeInput = driver.findElement(By.id("postal-code"));
+
+       firstNameInput.sendKeys("Samantha");
+       lastNameInput.sendKeys("Green");
+       postalCodeInput.sendKeys("1234");
+
+       WebElement btn_Continue = driver.findElement(By.id("continue"));
+       btn_Continue.click();
+
+       WebElement btn_Finish = driver.findElement(By.id("finish"));
+       btn_Finish.click();
+
+       WebElement confirmationHeader = driver.findElement(By.tagName("h2"));
+       String text = confirmationHeader.getText();
+
+        Assert.assertTrue(text.contains("Thank you"));
+
         }
 }
