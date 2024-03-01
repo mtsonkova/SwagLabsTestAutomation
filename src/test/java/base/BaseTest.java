e1f80f32913a68b0f294d7f2f27e3436ec658d75
mtsonkova <mtsonkova950@gmail.com>
2024-03-01 20:32:07 +0200

    In progress

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -7,16 +7,18 @@ import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
 protected WebDriver driver;
+
     @BeforeTest(alwaysRun = true)
     public void setUp() {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         String baseUrl = "https://www.saucedemo.com/";
         driver.get(baseUrl);
     }
-
-
     @AfterTest
     public void tearDown() {
         driver.quit();
     }
+
+    public void ReadPropertiesFile()
 }
