26cf02cf60f36aa2731fd984761914ea97063394
mtsonkova <mtsonkova950@gmail.com>
2024-03-06 16:32:02 +0200

    Added method initializeDriver. Values read from the globalConfig.properties file

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -9,16 +9,27 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.*;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Properties;
 
 public class BaseTest {
-protected static WebDriver driver;
+    protected static WebDriver driver;
+
     @BeforeMethod(alwaysRun = true)
-    public void setUp() {
+    public void setUp() throws IOException {
+        driver = initializeDriver();
+        /*
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
 
@@ -26,19 +37,54 @@ protected static WebDriver driver;
         driver.manage().window().maximize();
         String baseUrl = "https://www.saucedemo.com/";
         driver.get(baseUrl);
+
+        */
     }
+
+    public WebDriver initializeDriver() throws IOException {
+        Properties properties = new Properties();
+        FileInputStream input = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\utilities\\globalConfig.properties");
+        properties.load(input);
+
+        String browser = properties.getProperty("browser");
+        String baseUrl = properties.getProperty("baseUrl");
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+
+            driver = new ChromeDriver(chromeOptions);
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.addArguments("--headless");
+
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.addArguments("--headless");
+
+            driver = new EdgeDriver();
+        }
+
+
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+
+        return driver;
+    }
+
     @AfterMethod
     public void tearDown() {
         driver.quit();
     }
 
     public static File getScreenshot(String testCaseName, WebDriver driver) throws IOException {
-        TakesScreenshot ts = (TakesScreenshot)driver;
+        TakesScreenshot ts = (TakesScreenshot) driver;
         File source = ts.getScreenshotAs(OutputType.FILE);
 
         String destPath = "D:\\Projects\\SwagLabsTestAutomation\\Screenshots\\" + testCaseName + ".png";
         File file = new File(destPath);
-       Files.copy(source.toPath(), file.toPath());
-       return file;
+        Files.copy(source.toPath(), file.toPath());
+        return file;
     }
 }
