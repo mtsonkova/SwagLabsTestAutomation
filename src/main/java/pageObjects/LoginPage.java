80d5f9ba48f9e99f60c66e234c6288c52f9e5fac
mtsonkova <mtsonkova950@gmail.com>
2024-03-09 17:44:16 +0200

    Finished test cases for LoginPage. All test cases executed and passed

--- src/main/java/pageObjects/LoginPage.java
+++ src/main/java/pageObjects/LoginPage.java
@@ -1,15 +1,45 @@
 package pageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-    WebDriver driver;
+    private WebDriver driver;
+
+    @FindBy(id = "user-name")
+    WebElement userNamePlaceholder;
+
+    @FindBy(id = "password")
+    WebElement passwordPlaceholder;
+
+    @FindBy(id = "login-button")
+    WebElement btn_Login;
+
+    @FindBy (css = "div h3")
+    WebElement errMsgContainer;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
+    public void enterUserName(String userName) {
+        userNamePlaceholder.sendKeys(userName);
+    }
+
+    public void enterPassword(String password) {
+        passwordPlaceholder.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        btn_Login.click();
+    }
+
+    public String getErrorMessage() {
+        String errorMessage = errMsgContainer.getText();
+        return errorMessage;
+    }
 
 }
---
+++ src/test/java/LoginPageTest.java
@@ -0,0 +1,63 @@
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pageObjects.LoginPage;
+
+public class LoginPageTest extends BaseTest {
+
+    LoginPage loginPage;
+   @BeforeMethod
+   public void setTest() {
+      loginPage = new LoginPage(driver);
+   }
+    @Test
+    public void LoginWithValidUserNameAndPasswordAndCheckPageTitle() {
+       loginPage.enterUserName("standard_user");
+       loginPage.enterPassword("secret_sauce");
+       loginPage.clickLoginButton();
+       String expectedUrl = "https://www.saucedemo.com/inventory.html";
+       String actualUrl = driver.getCurrentUrl();
+       Assert.assertEquals(actualUrl, expectedUrl);
+    }
+
+    @Test(enabled = true)
+    public void LoginWithWrongUserNameAndValidPasswordAndCheckErrorMessage() {
+        loginPage.enterUserName("wrong_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        String expectedError = "Epic sadface: Username and password do not match any user in this service";
+        String actualError = loginPage.getErrorMessage();
+        Assert.assertEquals(actualError, expectedError);
+    }
+
+    @Test
+    public void LoginWithLockedOutUserAndValidPasswordAndCheckErrorMessage() {
+        loginPage.enterUserName("locked_out_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        String expectedError = "Epic sadface: Sorry, this user has been locked out.";
+        String actualError = loginPage.getErrorMessage();
+        Assert.assertEquals(actualError, expectedError);
+    }
+
+    @Test
+    public void LoginWithEmptyUserNameAndValidPasswordAndCheckErrorMessage(){
+        loginPage.enterUserName("");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        String expectedError = "Epic sadface: Username is required";
+        String actualError = loginPage.getErrorMessage();
+        Assert.assertEquals(actualError, expectedError);
+    }
+    @Test
+    public void LoginWithValidUserNameAndEmptyPasswordAndCheckErrorMessage(){
+        loginPage.enterUserName("standard_user");
+        loginPage.enterPassword("");
+        loginPage.clickLoginButton();
+        String expectedError = "Epic sadface: Password is required";
+        String actualError = loginPage.getErrorMessage();
+        Assert.assertEquals(actualError, expectedError);
+    }
+
+}
