b9a584052a9b392874ba38ee47640c1e1f098b6f
mtsonkova <mtsonkova950@gmail.com>
2024-03-28 22:43:26 +0200

    Added CheckoutInformation class in pageObjects package

--- src/main/java/pageObjects/CheckoutInformation.java
+++ src/main/java/pageObjects/CheckoutInformation.java
@@ -1,6 +1,8 @@
 package pageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CheckoutInformation {
@@ -10,4 +12,47 @@ public class CheckoutInformation {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+
+    @FindBy(id = "first-name")
+    WebElement firstNamePlaceholder;
+
+    @FindBy(id = "last-name")
+    WebElement lastNamePlaceholder;
+
+    @FindBy(id = "postal-code")
+    WebElement postalCodePlaceholder;
+
+    @FindBy(css = "div#error-message-container h3")
+    WebElement errorMsgContainer;
+
+    @FindBy(id = "cancel")
+    WebElement btnCancel;
+
+    @FindBy(id = "continue")
+    WebElement btnContinue;
+
+    public void enterFirstName(String firstName) {
+        firstNamePlaceholder.sendKeys(firstName);
+    }
+
+    public void enterLastName(String lastName) {
+        lastNamePlaceholder.sendKeys(lastName);
+    }
+
+    public void enterPostCode(String postCode) {
+        postalCodePlaceholder.sendKeys(postCode);
+    }
+
+    public void clickOnCancelBtn() {
+        btnCancel.click();
+    }
+
+    public void clickOnContinueBtn() {
+        btnContinue.click();
+    }
+
+    public String getErrMsgText() {
+        return errorMsgContainer.getText();
+    }
+
 }
