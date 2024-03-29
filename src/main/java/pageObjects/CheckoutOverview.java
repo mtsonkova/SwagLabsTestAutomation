e901dfd8526ad9eedc52007dc8c22129e2625ac1
mtsonkova <mtsonkova950@gmail.com>
2024-03-29 22:34:40 +0200

    CheckoutOverview class completed

--- src/main/java/pageObjects/CheckoutOverview.java
+++ src/main/java/pageObjects/CheckoutOverview.java
@@ -1,7 +1,12 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class CheckoutOverview {
     private WebDriver driver;
@@ -10,6 +15,65 @@ public class CheckoutOverview {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+
+    @FindBy(className = "cart_list")
+    List<WebElement> purchasedProductsList;
+
+    @FindBy(className = "summary_info_label")
+    List<WebElement> orderInfoSummaryElements;
+
+    @FindBy(className = "summary_subtotal_label")
+    WebElement subTotalPrice;
+
+    @FindBy(className = "summary_tax_label")
+    WebElement taxPrice;
+
+    @FindBy(className = "summary_total_label")
+    WebElement totalPrice;
+
+    @FindBy(id = "cancel")
+    WebElement btnCancel;
+
+    @FindBy(id = "finish")
+    WebElement btnFinish;
+
+    public String getPaymentInformation() {
+        WebElement element = orderInfoSummaryElements.get(0).findElement(By.className("summary_value_label"));
+        return element.getText();
+    }
+
+    public String getShippingInformation() {
+        WebElement element = orderInfoSummaryElements.get(1).findElement(By.className("summary_value_label"));
+        return element.getText();
+    }
+
+    public double getItemTotal() {
+        String[] tokens = subTotalPrice.getText().split("$");
+        double subTotalPrice = Double.parseDouble(tokens[1]);
+        return subTotalPrice;
+    }
+
+    public double getTax() {
+        String[] tokens = taxPrice.getText().split("$");
+        double tax = Double.parseDouble(tokens[1]);
+        return tax;
+    }
+
+    public double getTotalPrice() {
+        String[] tokens = totalPrice.getText().split("$");
+        double totalPrice = Double.parseDouble(tokens[1]);
+        return totalPrice;
+    }
+
+    public void clickCancelBtn() {
+        btnCancel.click();
+    }
+
+    public void clickFinishBtn() {
+        btnFinish.click();
+    }
+
+
 
 
 }
