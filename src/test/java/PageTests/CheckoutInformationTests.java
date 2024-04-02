ca2d03aa1f719499a1ebd9be65be20bf07b6673d
mtsonkova <mtsonkova950@gmail.com>
2024-04-02 22:58:26 +0300

    All test cases for CheckoutInformationPageTests class are ready

--- src/test/java/PageTests/CheckoutInformationTests.java
+++ src/test/java/PageTests/CheckoutInformationTests.java
@@ -16,11 +16,12 @@ public class CheckoutInformationTests extends BaseTest {
 
     private JSONObject jsonObject = getJsonObject();
     JSONArray products = (JSONArray) jsonObject.get("purchasedProductsList");
-
     private String firstName = jsonObject.get("firstName").toString();
     private String lastName = jsonObject.get("lastName").toString();
     private String postCode = jsonObject.get("postalCode").toString();
 
+    private String actualErrMsg = null;
+    private String expectedErrMsg = null;
 
     @BeforeMethod
     public void setTest() {
@@ -36,6 +37,44 @@ public class CheckoutInformationTests extends BaseTest {
         checkoutInformationPage.enterInformationData(firstName, lastName, postCode);
         CheckoutOverviewPage checkoutOverviewPage = checkoutInformationPage.clickOnContinueBtn();
         String expectedUrl = "https://www.saucedemo.com/checkout-step-two.html";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl);
+    }
+
+    @Test
+    public void provideEmptyFirstNameAndCheckErrorMsg() {
+        firstName = "";
+        checkoutInformationPage.enterInformationData(firstName, lastName, postCode);
+        CheckoutOverviewPage checkoutOverviewPage = checkoutInformationPage.clickOnContinueBtn();
+        actualErrMsg = checkoutInformationPage.getErrMsgText();
+        expectedErrMsg = "Error: First Name is required";
+        Assert.assertEquals(actualErrMsg, expectedErrMsg);
+    }
+
+    @Test
+    public void provideEmptyLastNameAndCheckErrorMsg() {
+        lastName = "";
+        checkoutInformationPage.enterInformationData(firstName, lastName, postCode);
+        CheckoutOverviewPage checkoutOverviewPage = checkoutInformationPage.clickOnContinueBtn();
+        actualErrMsg = checkoutInformationPage.getErrMsgText();
+        expectedErrMsg = "Error: Last Name is required";
+        Assert.assertEquals(actualErrMsg, expectedErrMsg);
+    }
+
+    @Test
+    public void provideEmptyPostCodeAndCheckErrorMsg() {
+        postCode = "";
+        checkoutInformationPage.enterInformationData(firstName, lastName, postCode);
+        CheckoutOverviewPage checkoutOverviewPage = checkoutInformationPage.clickOnContinueBtn();
+        actualErrMsg = checkoutInformationPage.getErrMsgText();
+        expectedErrMsg = "Error: Postal Code is required";
+        Assert.assertEquals(actualErrMsg, expectedErrMsg);
+    }
+
+    @Test
+    public void clickOnCancelButton() {
+        checkoutInformationPage.clickOnCancelBtn();
+        String expectedUrl = "https://www.saucedemo.com/cart.html";
         String actualUrl = driver.getCurrentUrl();
         Assert.assertEquals(actualUrl, expectedUrl);
     }
